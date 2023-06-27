3cbf565539915e0361aa4fa7cdb70f3d7b1448c0
Darryl Ng <darryl1975@hotmail.com>
2023-06-27 09:34:11 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/paf_day24workshop_jul2023/PafDay24workshopJul2023Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day24workshop_jul2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay24workshopJul2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay24workshopJul2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf_day24workshop_jul2023/PafDay24workshopJul2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day24workshop_jul2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay24workshopJul2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
